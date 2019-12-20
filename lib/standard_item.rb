03483a28863a50e2754e229f663644717b718935
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-12-20 16:01:02 +0000

    Add methods to reduce sell_in and stop quality fall below 0

--- lib/standard_item.rb
+++ lib/standard_item.rb
@@ -1,9 +1,23 @@
 class StandardItem < Item
 
+  def update_properties
+    update_quality
+
+    @sell_in -= 1
+  end
+
+  private
+
   def update_quality
     return unless @quality.positive?
 
-    @quality -= 1
+    @quality -= decrease_quality
+  end
+
+  def decrease_quality
+    return 1 if @quality == 1 || @sell_in.positive?
+
+    2
   end
 
 end
--- spec/standard_item_spec.rb
+++ spec/standard_item_spec.rb
@@ -2,13 +2,38 @@ require 'standard_item'
 
 describe StandardItem do
   subject(:standard_item) { described_class.new('+5 Dexterity Vest', 20, 10) }
+  subject(:zero_quality_item) { described_class.new('+5 Dexterity Vest', 1, 0) }
+  subject(:negative_sell_in) { described_class.new('+5 Dexterity Vest', -1, 3) }
 
-  it 'knows its quality' do
-    expect(standard_item.quality).to eq 10
+  context '#quality' do
+    it 'knows its quality' do
+      expect(standard_item.quality).to eq 10
+    end
+
+    it 'adjusts quality' do
+      standard_item.update_properties
+      expect(standard_item.quality).to eq 9
+    end
+
+    it 'does not reduce quality to < 0' do
+      zero_quality_item.update_properties
+      expect(zero_quality_item.quality).to eq 0
+    end
+
+    it 'reduces quality at double the pace after sell by date expired' do
+      negative_sell_in.update_properties
+      expect(negative_sell_in.quality).to eq 1
+    end
   end
 
-  it 'adjusts quality' do
-    standard_item.update_quality
-    expect(standard_item.quality).to eq 9
+  context '#sell_in' do
+    it 'knows its sell_in' do
+      expect(standard_item.sell_in).to eq 20
+    end
+
+    it 'reduces its sell_in by 1' do
+      standard_item.update_properties
+      expect(standard_item.sell_in).to eq 19
+    end
   end
 end
