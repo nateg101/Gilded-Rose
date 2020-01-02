9f1cd91eeb632ce1cf3e0de176491e0a22a911a1
Nat Elliott-Green <natelliottgreen34@gmail.com>
2020-01-02 13:41:46 +0000

    Refactored gilded_rose to update properties for all items bar legendary

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,5 +1,4 @@
 require 'gilded_rose'
-require 'item'
 
 describe GildedRose do
 
@@ -36,11 +35,6 @@ describe GildedRose do
 
 
   context "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-    end
 
     it 'calls update_properties on conjured_item' do
       expect(conjured_item).to receive (:update_properties)
